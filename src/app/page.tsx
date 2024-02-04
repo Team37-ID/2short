2985b20491c8c6077622d37f4d5db9a94016b167
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-04 20:41:38 +0700

    refactor(comps): url input in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,8 @@ import URLInput from "@/components/ui/url-input"
 
 export default function Home() {
 	return (
-		<main className="flex flex-col gap-2 min-h-screen items-center justify-center md:flex-row">
+		<main className="flex flex-col min-h-screen items-center justify-center">
 			<URLInput />
-			<button
-				type="button"
-				className="btn btn-primary w-full max-w-sm md:w-auto"
-			>
-				Create
-			</button>
 		</main>
 	)
 }
--- src/components/ui/url-input.tsx
+++ src/components/ui/url-input.tsx
@@ -1,10 +1,18 @@
 const URLInput = () => {
 	return (
-		<input
-			type="url"
-			placeholder="Paste long URL here"
-			className="input input-bordered w-full max-w-sm"
-		/>
+		<div className="flex flex-col gap-2 md:flex-row">
+			<input
+				type="url"
+				placeholder="Paste long URL here"
+				className="input input-bordered w-full max-w-md"
+			/>
+			<button
+				type="button"
+				className="btn btn-primary w-full max-w-sm md:w-auto"
+			>
+				Create
+			</button>
+		</div>
 	)
 }
 
