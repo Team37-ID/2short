64c6bc94a17ed7cf71a8018d14a8acb8f2c0c03c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-04 15:27:44 +0700

    feat(comps): add url input

---
+++ src/components/ui/url-input.tsx
@@ -0,0 +1,11 @@
+const URLInput = () => {
+	return (
+		<input
+			type="url"
+			placeholder="Paste long URL here"
+			className="input input-bordered w-full max-w-sm"
+		/>
+	)
+}
+
+export default URLInput
