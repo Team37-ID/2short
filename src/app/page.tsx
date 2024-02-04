88fb521de35e4d3b2a5820d02c77d793bdc1a63d
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-04 16:03:42 +0700

    style: adjust button max width in mobile view to sm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
 	return (
 		<main className="flex flex-col gap-2 min-h-screen items-center justify-center md:flex-row">
 			<URLInput />
-			<button type="button" className="btn btn-primary w-full md:w-auto">
+			<button
+				type="button"
+				className="btn btn-primary w-full max-w-sm md:w-auto"
+			>
 				Create
 			</button>
 		</main>
