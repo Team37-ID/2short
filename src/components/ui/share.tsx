efcb50e2b6a1864c01ff2f64c1f4c79016ee40ac
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-04 22:01:14 +0700

    feat: add toast to show copy successful

--- src/components/ui/share.tsx
+++ src/components/ui/share.tsx
@@ -2,25 +2,40 @@
 
 import { SendDiagonalSolid } from "iconoir-react/solid"
 import { useCopyToClipboard } from "usehooks-ts"
+import { useState } from "react"
 
 const Share = () => {
 	const [copiedText, copy] = useCopyToClipboard()
+	const [showToast, setShowToast] = useState<boolean>(false)
 
 	const handleCopy = (text: string) => {
 		copy(text)
-			.then(() => console.log(`${copiedText} copied to clipboard`))
+			.then(() => {
+				setShowToast(true)
+				console.log(`${copiedText} copied to clipboard`)
+				setTimeout(() => setShowToast(false), 3000)
+			})
 			.catch(() => console.log("Failed to copy"))
 	}
 
 	return (
-		<button
-			type="button"
-			className="flex flex-row justify-center items-center gap-2 border py-2 px-4 rounded-lg bg-gradient-to-br from-[#202020]/35 to-[#202020]/5 backdrop-blur hover:bg-primary transition-colors duration-300"
-			onClick={() => handleCopy(window.location.href)}
-		>
-			<p className="text-white md:text-base text-sm">Share</p>
-			<SendDiagonalSolid className="w-6 md:w-full" />
-		</button>
+		<div>
+			<button
+				type="button"
+				className="flex flex-row justify-center items-center gap-2 border py-3 px-4 rounded-lg bg-gradient-to-br from-[#202020]/35 to-[#202020]/5 backdrop-blur hover:bg-primary transition-colors duration-300"
+				onClick={() => handleCopy(window.location.href)}
+			>
+				<p className="text-white md:text-base text-sm">Share</p>
+				<SendDiagonalSolid className="w-6 md:w-full" />
+			</button>
+			{showToast && (
+				<div className="toast toast-end">
+					<div className="alert bg-green-500">
+						<p className="text-white">Link copied to clipboard!</p>
+					</div>
+				</div>
+			)}
+		</div>
 	)
 }
 
