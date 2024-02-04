17e2f89577da7388bc2aa7aff826a5bad104c84f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-04 15:24:48 +0700

    feat: add tooltip and change swap animation to flip

--- src/components/ui/toggle-dark.tsx
+++ src/components/ui/toggle-dark.tsx
@@ -4,16 +4,22 @@ import { HalfMoon, SunLight } from "iconoir-react"
 
 const ToggleDark = () => {
 	return (
-		<label className="swap swap-rotat">
+		<label className="swap swap-flip">
 			<input
 				type="checkbox"
 				className="theme-controller"
 				value="halloween"
 			/>
-			<div className="swap-on border flex items-center justify-center p-3 rounded-lg bg-gradient-to-br from-orange-600 to-yellow-400 backdrop-blur-lg">
+			<div
+				data-tip="Toggle dark mode"
+				className="tooltip tooltip-bottom swap-on border flex items-center justify-center p-3 rounded-lg bg-gradient-to-br from-orange-600 to-yellow-400 backdrop-blur-lg"
+			>
 				<SunLight className="fill-current w-6 md:w-full" />
 			</div>
-			<div className="swap-off border flex items-center justify-center p-3 rounded-lg bg-gradient-to-br from-purple-900 to-violet-500 backdrop-blur-lg">
+			<div
+				data-tip="Toggle light mode"
+				className="tooltip tooltip-bottom swap-off border flex items-center justify-center p-3 rounded-lg bg-gradient-to-br from-purple-900 to-violet-500 backdrop-blur-lg"
+			>
 				<HalfMoon className="fill-current w-6 md:w-full" />
 			</div>
 		</label>
