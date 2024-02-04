2ae3aa40b1cb12e10719d5b2c04efae69c916309
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-04 21:49:24 +0700

    feat: add copy text to share

--- src/components/ui/share.tsx
+++ src/components/ui/share.tsx
@@ -1,10 +1,20 @@
 import { SendDiagonalSolid } from "iconoir-react/solid"
+import { useCopyToClipboard } from "usehooks-ts"
 
 const Share = () => {
+	const [copiedText, copy] = useCopyToClipboard()
+
+	const handleCopy = (text: string) => {
+		copy(text)
+			.then(() => console.log(`${copiedText} copied to clipboard`))
+			.catch(() => console.log("Failed to copy"))
+	}
+
 	return (
 		<button
 			type="button"
 			className="flex flex-row justify-center items-center gap-2 border py-2 px-4 rounded-lg bg-gradient-to-br from-[#202020]/35 to-[#202020]/5 backdrop-blur hover:bg-primary transition-colors duration-300"
+			onClick={() => handleCopy(window.location.href)}
 		>
 			<p className="text-white md:text-base text-sm">Share</p>
 			<SendDiagonalSolid className="w-6 md:w-full" />
