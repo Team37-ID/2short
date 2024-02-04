fd27eb9e7d43b67ff795dfa3e8e4f64af88db9b2
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-02-04 22:43:41 +0700

    style: adjust text input width for different layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import URLInput from "@/components/ui/url-input"
 
 export default function Home() {
 	return (
-		<main className="flex flex-col min-h-screen items-center justify-center">
+		<main className="flex flex-col min-h-screen justify-center">
 			<URLInput />
 		</main>
 	)
